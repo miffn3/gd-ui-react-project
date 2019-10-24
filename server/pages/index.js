7db7f863da376a4fdef9ddfaa94923ad2e943f99
Mikhail <miffn3@yandex.ru>
2019-10-24 23:14:47 +0400

    fix: server index.js

---
+++ server/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import id from '../pages/api/card/[id]';
+import {cards} from '../pages/api/cards';
+
+function Home() {
+  return <div> </div>
+}
+
+export default Home
