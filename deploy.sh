7b7dd07fe2ef7eff569bf3bc60f6007dd663ac18
EasileleM <pro100prosavva@gmail.com>
2019-10-25 11:14:28 +0400

    test

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,12 @@ FRONTEND_TARGET="FRONTEND"
 if [ $APP_SIDE = $FRONTEND_TARGET ]
   then
     cd client
+    npm install
+    npm run build
 else
     cd server
+    npm install
+    npm run build
+    npm run start
 fi
 
-npm install
-npm run build
-npm start
