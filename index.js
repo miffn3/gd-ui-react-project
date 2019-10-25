7477b09f262dfb05daf87061169cedac83ee5e97
EasileleM <pro100prosavva@gmail.com>
2019-10-25 12:20:27 +0400

    test

--- index.js
+++ index.js
@@ -9,5 +9,13 @@ app.use(cors());
 app.get('/', (request, response) => {
 })
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
