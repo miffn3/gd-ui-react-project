68ad40fed547ce0c4998dcee3dcfbfe18eeb133a
Mikhail <miffn3@yandex.ru>
2019-10-25 14:00:56 +0400

    test9

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# FRONTEND_TARGET="FRONTEND"
+FRONTEND_TARGET="FRONTEND"
 
-# if [ $APP_SIDE = $FRONTEND_TARGET ]
-#   then
-cd client
-# else
-#     cd server
-# fi
+if [ $APP_SIDE = $FRONTEND_TARGET ]
+  then
+    cd client
+else
+    cd server
+fi
 
 npm install
 npm run build
-# npm run start $PORT
+npm run start
